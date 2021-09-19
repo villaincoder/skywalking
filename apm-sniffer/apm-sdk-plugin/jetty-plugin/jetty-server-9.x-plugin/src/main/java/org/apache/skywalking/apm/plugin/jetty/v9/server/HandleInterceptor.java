1df9db0f2752ee6736ce5d1a9ec3568f072f6b86
villaincoder <zhangtian1993@gmail.com>
2021-09-19 17:19:01 +0800

    Fix: jetty plugin status code incorrect

--- apm-sniffer/apm-sdk-plugin/jetty-plugin/jetty-server-9.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/jetty/v9/server/HandleInterceptor.java
+++ apm-sniffer/apm-sdk-plugin/jetty-plugin/jetty-server-9.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/jetty/v9/server/HandleInterceptor.java
@@ -32,6 +32,7 @@ import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceM
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.MethodInterceptResult;
 import org.apache.skywalking.apm.agent.core.util.MethodUtil;
 import org.apache.skywalking.apm.network.trace.component.ComponentsDefine;
+import org.apache.skywalking.apm.plugin.jetty.v9.server.define.JettyInstrumentation;
 import org.eclipse.jetty.server.HttpChannel;
 
 public class HandleInterceptor implements InstanceMethodsAroundInterceptor {
@@ -47,36 +48,39 @@ public class HandleInterceptor implements InstanceMethodsAroundInterceptor {
     @Override
     public void beforeMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes,
         MethodInterceptResult result) throws Throwable {
-        HttpChannel httpChannel = (HttpChannel) objInst;
-        HttpServletRequest servletRequest = httpChannel.getRequest();
+        if (JettyInstrumentation.METHOD_HANDLE.equals(method.getName())) {
+            HttpChannel httpChannel = (HttpChannel) objInst;
+            HttpServletRequest servletRequest = httpChannel.getRequest();
 
-        ContextCarrier contextCarrier = new ContextCarrier();
+            ContextCarrier contextCarrier = new ContextCarrier();
 
-        CarrierItem next = contextCarrier.items();
-        while (next.hasNext()) {
-            next = next.next();
-            next.setHeadValue(servletRequest.getHeader(next.getHeadKey()));
+            CarrierItem next = contextCarrier.items();
+            while (next.hasNext()) {
+                next = next.next();
+                next.setHeadValue(servletRequest.getHeader(next.getHeadKey()));
+            }
+
+            AbstractSpan span = ContextManager.createEntrySpan(servletRequest.getRequestURI(), contextCarrier);
+            Tags.URL.set(span, servletRequest.getRequestURL().toString());
+            Tags.HTTP.METHOD.set(span, servletRequest.getMethod());
+            span.setComponent(ComponentsDefine.JETTY_SERVER);
+            SpanLayer.asHttp(span);
+        } else if (JettyInstrumentation.METHOD_ON_COMPLETED.equals(method.getName())) {
+            HttpChannel httpChannel = (HttpChannel) objInst;
+            HttpServletResponse servletResponse = httpChannel.getResponse();
+            AbstractSpan span = ContextManager.activeSpan();
+            if (IS_SERVLET_GET_STATUS_METHOD_EXIST && servletResponse.getStatus() >= 400) {
+                span.errorOccurred();
+                Tags.STATUS_CODE.set(span, Integer.toString(servletResponse.getStatus()));
+            }
+            ContextManager.stopSpan();
+            ContextManager.getRuntimeContext().remove(Constants.FORWARD_REQUEST_FLAG);
         }
-
-        AbstractSpan span = ContextManager.createEntrySpan(servletRequest.getRequestURI(), contextCarrier);
-        Tags.URL.set(span, servletRequest.getRequestURL().toString());
-        Tags.HTTP.METHOD.set(span, servletRequest.getMethod());
-        span.setComponent(ComponentsDefine.JETTY_SERVER);
-        SpanLayer.asHttp(span);
     }
 
     @Override
     public Object afterMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes,
         Object ret) throws Throwable {
-        HttpChannel httpChannel = (HttpChannel) objInst;
-        HttpServletResponse servletResponse = httpChannel.getResponse();
-        AbstractSpan span = ContextManager.activeSpan();
-        if (IS_SERVLET_GET_STATUS_METHOD_EXIST && servletResponse.getStatus() >= 400) {
-            span.errorOccurred();
-            Tags.STATUS_CODE.set(span, Integer.toString(servletResponse.getStatus()));
-        }
-        ContextManager.stopSpan();
-        ContextManager.getRuntimeContext().remove(Constants.FORWARD_REQUEST_FLAG);
         return ret;
     }
 
